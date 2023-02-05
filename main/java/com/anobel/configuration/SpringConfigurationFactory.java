59b85ee481074bcaf3fa9fa213ccf78baa5e69f3
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-02-05 20:08:58 +0200

    added front end | controller modified

---
+++ main/java/com/anobel/configuration/SpringConfigurationFactory.java
@@ -0,0 +1,8 @@
+package com.anobel.configuration;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringConfigurationFactory {
+
+}
--- main/java/com/anobel/controller/testController.java
+++ main/java/com/anobel/controller/testController.java
@@ -1,5 +1,9 @@
 package com.anobel.controller;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -10,19 +14,36 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @RequestMapping("/test")
 public class testController {
+    private static final Logger logger = LoggerFactory.getLogger(testController.class);
     @Autowired
     private ApplicationContext applicationContext;
 
     @GetMapping("/hello")
     @ResponseBody
     public String hello(@RequestParam(value = "name",defaultValue = "Max") String name){
+        logger.info("https://192.168.0.103:8089/test/hello");
         return String.format("Hello %s",name);
+    }
+    @RequestMapping("/index")
+    @ResponseBody
+    public String mainPage(){
+        logger.info("https://192.168.0.103:8089/test/index");
+        return "templates/Login";
     }
 
     @RequestMapping(value = "stop",method = RequestMethod.GET,produces = MediaType.APPLICATION_JSON_VALUE)
     @ResponseBody
     public void stop(){
+        logger.info("https://192.168.0.103:8089/test/stop");
         ((ConfigurableApplicationContext)applicationContext).close();
+    }
+    @PreDestroy
+    public void processEnd(){
+        logger.info("Pre derstroy called");
+    }
+    @PostConstruct
+    public void processStart(){
+        logger.info("Post construct called");
     }
 
 }
