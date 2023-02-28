2ae1ba7990dd5db147fb62f700ec553f84732a6b
Maxym-Denysov <adamsmatthew70@yahoo.com>
2023-02-28 17:19:10 +0200

    ram price history entity

--- main/java/com/anobel/model/price/Ram_price_history.java
+++ main/java/com/anobel/model/price/Ram_price_history.java
@@ -1,2 +1,29 @@
-package com.anobel.model.price;public class Ram_price_history {
+package com.anobel.model.price;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "ram_price_history")
+public class Ram_price_history {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id",nullable = false)
+    private Long id;
+    @Column(name = "start_price")
+    private float start_price;
+    @Column(name = "last_price")
+    private float last_price;
+    @Column(name = "start_price_date")
+    private LocalDateTime start_price_date;
+    @Column(name = "last_price_date")
+    private LocalDateTime last_price_date;
+
 }
