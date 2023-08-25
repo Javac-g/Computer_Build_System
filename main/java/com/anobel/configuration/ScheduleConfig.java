f8e5a805f9ad0bd457747c04fc3c64c4acbef82a
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-08-26 00:49:56 +0300

    Update ScheduleConfig.java

--- main/java/com/anobel/configuration/ScheduleConfig.java
+++ main/java/com/anobel/configuration/ScheduleConfig.java
@@ -2,10 +2,26 @@
 @EnableScheduling
 public class ScheduleConfig {
     @Autowired
-    private PriceHistoryService priceHistoryService;
+    private Cpu_price_history cpu_price_history;
     
     @Scheduled(cron = "0 0 * * * *")
-    public void updatePriceHistory() {
-        priceHistoryService.updatePriceHistory();
+    public void updateCpuPriceHistory() {
+        cpu_price_history.updatePriceHistory();
+    }
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateGpuPriceHistory() {
+        cpu_price_history.updatePriceHistory();
+    }
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateRamPriceHistory() {
+        cpu_price_history.updatePriceHistory();
+    }
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateMotherboardPriceHistory() {
+        cpu_price_history.updatePriceHistory();
+    }
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateStoragePriceHistory() {
+        cpu_price_history.updatePriceHistory();
     }
 }
