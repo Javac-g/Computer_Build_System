bff00c38c9da781f5de3e4ca515febe94cfa972b
Denysov Maksym <93475429+Javac-g@users.noreply.github.com>
2023-08-26 01:05:12 +0300

    Update ScheduleConfig.java

--- main/java/com/anobel/configuration/ScheduleConfig.java
+++ main/java/com/anobel/configuration/ScheduleConfig.java
@@ -1,6 +1,21 @@
 @Configuration
 @EnableScheduling
 public class ScheduleConfig {
+ 
+    @Autowired
+    private final Gpu_price_history gpu_price_history;
+    @Autowired
+    private final Motherboard_price_history motherboard_price_history;
+    @Autowired
+    private final Storage_price_history storage_price_history;
+    @Autowired
+    private final Ram_price_history ram_price_history;
+	@Autowired
+	private final ComputerCase_price_history computerCase_price_history;
+    @Autowired
+	private final PowerSupply_price_history powerSupply_price_history;
+    @Autowired
+	private final CpuCooler_price_history cpuCooler_price_history;
     @Autowired
     private Cpu_price_history cpu_price_history;
     
@@ -10,18 +25,31 @@ public class ScheduleConfig {
     }
     @Scheduled(cron = "0 0 * * * *")
     public void updateGpuPriceHistory() {
-        cpu_price_history.updatePriceHistory();
+        gpu_price_history.updatePriceHistory();
     }
     @Scheduled(cron = "0 0 * * * *")
     public void updateRamPriceHistory() {
-        cpu_price_history.updatePriceHistory();
+        ram_price_history.updatePriceHistory();
     }
     @Scheduled(cron = "0 0 * * * *")
     public void updateMotherboardPriceHistory() {
-        cpu_price_history.updatePriceHistory();
+        motherboard_price_history.updatePriceHistory();
     }
     @Scheduled(cron = "0 0 * * * *")
     public void updateStoragePriceHistory() {
-        cpu_price_history.updatePriceHistory();
+        storage_price_history.updatePriceHistory();
     }
+     @Scheduled(cron = "0 0 * * * *")
+    public void updateCpuCoolerPriceHistory() {
+        cpuCooler_price_history.updatePriceHistory();
+    }
+    @Scheduled(cron = "0 0 * * * *")
+    public void updateComputerCasePriceHistory() {
+        computerCase_price_history.updatePriceHistory();
+    }
+    @Scheduled(cron = "0 0 * * * *")
+    public void updatePowerSupplyPriceHistory() {
+        powerSuply_price_history.updatePriceHistory();
+    }
+  
 }
